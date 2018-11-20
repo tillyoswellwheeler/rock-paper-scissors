a02ccbc3219be5c5f189c6bcafa9923633445344
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-20 11:29:23 +0000

    Committing changes to the engine functions for DRY

--- js/engine.js
+++ js/engine.js
@@ -7,35 +7,23 @@ var won = 0;
 var lost = 0;
 var draw = 0;
 
+/* Listen for Player choice */
 
-/* Listen for the button clicks */
-
-$("button").click(function(event) {
+$('button').click(function(event) {
   var play_you = this.getAttribute("value");
+  return computer_thinks();
+  compare(play_you, play_machine);
+
 })
 
+/* Computer responds with choice */
 
-// $("#rock").click(function(event) {
-//   var play_you = 'rock';
-//   $("#play_you").text(play_you);
-//   var play_machine = computer_move();
-//   compare(play_you, play_machine);
-// });
-//
-// $("#paper").click(function(event) {
-//   var play_you = 'paper';
-//   $("#play_you").text(play_you);
-//   var play_machine = computer_move();
-//   compare(play_you, play_machine);
-// });
-//
-// $("#scissors").click(function(event) {
-//   var play_you = 'scissors';
-//   $("#play_you").text(play_you);
-//   var play_machine = computer_move();
-//   compare(play_you, play_machine);
-// });
+function computer_thinks() {
+  var play_machine = computer_move();
+  // compare(play_you, play_machine);
+}
 
+/* Calculate computer_move() math */
 
 function computer_move() {
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
@@ -44,6 +32,49 @@ function computer_move() {
   $("#play_machine").text(play_machine);
   return play_machine;
 }
+
+/* Compare two players choices */
+
+// function compare(you, machine){
+//   var verdict = 'lose';
+//
+//   if ( you == machine ) {
+//     verdict = 'draw';
+//   } else if ((you == "rock") && (machine == "scissors")) {
+//     verdict = 'win';
+//   } else if ((you == "paper") && (machine == "rock")) {
+//     verdict = 'win';
+//   } else if  ((you == "scissors") && (machine == "paper")) {
+//     verdict = 'win';
+//   }
+//
+//   $("#verdict").text(verdict);
+//
+// }
+//
+// function score(verdict) {
+//
+//   played++;
+//   $("#game_played").text(played);
+//
+//
+// switch (compare) {
+// case 'win':
+// won++;
+// $("#won").text(won);
+// break;
+// case 'lose':
+// lost++;
+// $("#lost").text(lost);
+// break;
+// case 'draw':
+// draw++;
+// $("#draw").text(draw);
+// break;
+//
+// }
+// }
+
 
 function compare(you, machine){
   var verdict = 'lose';
