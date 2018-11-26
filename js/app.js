c1abeda99054e445e2db2af762e17f9e9eebb016
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2018-11-26 13:44:51 +0000

    Committing changes to app.js code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -15,23 +15,53 @@ function getCompChoice() {
   return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+  if (letter == "r") return "Rock";
+  if (letter =="p") return "Paper";
+  return "Scissors";
+}
+
+function win(userChoice, compChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  compScore_span.innerHTML = compScore;
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(compChoice)}${smallCompWord}. You win`;
+}
+
+
+
+function lose(userChoice, compChoice) {
+  compScore++;
+  userScore_span.innerHTML = userScore;
+  compScore_span.innerHTML = compScore;
+  const smallUserWord = "user".fontsize(3).sup();
+  const smallCompWord = "comp".fontsize(3).sup();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(compChoice)}${smallCompWord}. You lost`;
+}
+
+function draw() {
+  console.log("draw");
+}
+
 function game(userChoice) {
   const compChoice = getCompChoice();
   switch (userChoice + compChoice) {
-    case "rp";
-    case "pr";
-    case "sp";
-      console.log("User wins.");
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, compChoice);
       break;
-    case "rp";
-    case "ps";
-    case "sr";
-      console.log("Comp wins");
+    case "rp":
+    case "ps":
+    case "sr":
+      lost(userChoice, compChoice);
       break;
-    case "rr";
-    case "pp";
-    case "ss";
-      console.log("It's a draw!");
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, compChoice);
       break;  
   }
 }
